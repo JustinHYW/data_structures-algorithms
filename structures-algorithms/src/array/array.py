671f83e337e59fb5551e82b4006b134435275053
shifa <shifa@FANASUS>
2022-08-16 16:03:06 -0700

    third time test

--- structures-algorithms/src/array/array.py
+++ structures-algorithms/src/array/array.py
@@ -2,3 +2,17 @@ cars = ["ford", "volvo", "bmw"]
 
 for  i in cars:
     print(i)
+    
+    
+shop_name = "fan's coffee"
+coffee_sizes = ["small", "medium", "large"]
+coffee_roasts = ["hot chocolate", "light", "medium", "dark", "espresso"]
+
+
+def order_coffee(size, roast):
+    
+    return "here's your {} coffee roasted {} ".format(size, roast)
+
+def add_milk(fat_content):
+    
+    return "I've added the {}% milk".format(fat_content)
